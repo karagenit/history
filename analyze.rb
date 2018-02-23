939cfe990696dcc9783a46eb78f93931a76c217d
Caleb Smith <karagenit@outlook.com>
2018-02-22 19:08:39 -0500

    Add Analysis Script

---
+++ analyze.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+data = {}
+
+Dir.glob('data/*').each do |file|
+  File.readlines(file).each do |line|
+    # TODO: substitute aliases
+    command = line.strip.split(' ')[0]
+    if data[command].nil?
+      data[command] = 1
+    else
+      data[command] += 1
+    end
+  end
+end
+
+p data
