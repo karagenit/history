09674afb281d504520ac826f4794c816391c0eb1
Caleb Smith <karagenit@outlook.com>
2018-02-22 19:10:06 -0500

    Improve Splitting

--- analyze.rb
+++ analyze.rb
@@ -5,7 +5,7 @@ data = {}
 Dir.glob('data/*').each do |file|
   File.readlines(file).each do |line|
     # TODO: substitute aliases
-    command = line.strip.split(' ')[0]
+    command = line.strip.split(/[\s;]/)[0]
     if data[command].nil?
       data[command] = 1
     else
