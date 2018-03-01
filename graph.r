9fd05c33ce469555f10aed2fddbc5aef90d0ca66
Caleb Smith <karagenit@outlook.com>
2018-02-28 21:41:40 -0500

    Add R Graphing Utility

---
+++ graph.r
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+
+library("rjson")
+
+# This is a list of dataframes
+data <- fromJSON(file = "results.json")
+
+# Magically converts this to one dataframe - thanks SO
+data = do.call("rbind", data)
+
+# Let's just plot the first 10 rows
+data = data[1:10,]
+
+x11()
+barplot(data, main = "Most Popular Bash Commands", ylab = "# of Recorded Uses in .bash_history")
+Sys.sleep(1000)
