c5535a317b8dc882e931262f514080fdb0c1494e
Caleb Smith <karagenit@outlook.com>
2018-03-16 14:52:29 -0400

    Plot Git Usage Over Time

--- graph.r
+++ graph.r
@@ -8,12 +8,28 @@ data <- fromJSON(file = "results.json")
 # Magically converts this to one dataframe - thanks SO
 top = do.call("rbind", data$totals)
 
-print(top);
-
 # Let's just plot the first 10 rows
 top = top[1:10,]
 
 x11()
 barplot(top, main = "Most Popular Bash Commands", ylab = "# of Recorded Uses in .bash_history")
 dev.print(png, "results.png", width=500, height=500)
+
+# Delete the totals - we only want the dates
+data$totals <- NULL
+gitvals = list()
+
+#print(data[[]][["git"]])
+
+# Grab all the 'git' values from the dates
+for (key in names(data)) {
+    gitvals[[key]] <- data[[key]][["git"]]
+}
+
+# Prepare for Graphing
+gitvals = unlist(gitvals)
+
+x11()
+barplot(gitvals, main = "Git Usage Over Time")
+
 Sys.sleep(1000)
