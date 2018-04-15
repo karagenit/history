7d0e070b7b3ed3d294ada139b8bcb468d28360d0
Caleb Smith <karagenit@outlook.com>
2018-04-15 14:19:58 -0400

    Fix Status Script Error Handling

--- status.sh
+++ status.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-unsaved=`wc -l ~/.bash_history | cut -f1 -d' '`
+# Counts the lines in .bash_history
+unsaved=`wc -l ~/.bash_history 2>/dev/null`
+
+# We get an error if .bash_history doesn't exist, so this handles that
+if [ $? == 0 ]
+then
+    unsaved=`echo $unsaved | cut -f1 -d' '`
+else
+    unsaved=0
+fi
 
 echo "Currently $unsaved Unsaved Entries"
