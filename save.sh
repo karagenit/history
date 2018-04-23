3f935427341a648fe2d81e15ee0a8872177b577f
Caleb Smith <karagenit@outlook.com>
2018-04-22 22:45:04 -0400

    Make Save Script Portable

--- save.sh
+++ save.sh
@@ -3,4 +3,4 @@
 DATE=`date '+%Y-%m-%d %H:%M'`
 NAME="${DATE}.txt"
 
-mv -n "/home/caleb/.bash_history" "/home/caleb/documents/programming/src/history/data/${NAME}"
+mv -n "$HOME/.bash_history" "./data/${NAME}"
