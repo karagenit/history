cf7a232b4accce0ade4a3b8bcdbc0784eacaa935
Caleb Smith <karagenit@outlook.com>
2018-05-10 16:12:39 -0400

    Fix Save Script Run Path
    Cron runs the script from /home/caleb, which is a problem for saving to ./data/

--- save.sh
+++ save.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# Run Script from Real Script Location Always
+cd "$(dirname "$(realpath "$0")")"
+
 DATE=`date '+%Y-%m-%d %H:%M'`
 NAME="${DATE}.txt"
 
 mv -n "$HOME/.bash_history" "./data/${NAME}"
+echo "Saved $HOME/.bash_history to ./data/${NAME}"
