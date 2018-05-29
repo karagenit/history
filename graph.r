d90dd1ce93e8c7b4de198d5225dea1104d66a263
Caleb Smith <karagenit@outlook.com>
2018-05-28 22:21:55 -0400

    Change Graph Color/Fill

--- graph.r
+++ graph.r
@@ -7,7 +7,11 @@ totals = do.call("rbind", totals)
 top = totals[1:10,]
 
 x11()
-barplot(top, main = "Most Popular Bash Commands", ylab = "# of Recorded Uses in .bash_history")
+barplot(top,
+        main = "Most Popular Bash Commands",
+        ylab = "# of Recorded Uses in .bash_history",
+        col = "black",
+        density = 30)
 #dev.print(png, "results.png", width=600, height=500)
 
 dates <- fromJSON(file = "dates.json")
